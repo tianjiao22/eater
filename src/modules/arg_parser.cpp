d77f3e9ca09df8d72d4a2165a4f42269d3db4719
LiTianjiao <litj@thundersoft.com>
2020-11-04 20:26:56 +0800

    Modify interface test ok.

--- src/modules/arg_parser.cpp
+++ src/modules/arg_parser.cpp
@@ -1,7 +1,11 @@
 #include "modules/arg_parser.h"
 #include<stdio.h>
 #include <string.h>
-
+#include<stdlib.h>
+char *paddr = nullptr;
+char *pPort = nullptr;
+ int Ipflag =0,Portflag =0;
+int portnum = 0;
 int arg_parse(int argc, const char **argv)
 {
 	// TODO
@@ -9,35 +13,60 @@ int arg_parse(int argc, const char **argv)
         return 1; //若addr为空
     }
 
-    int len = strlen(*argv);
-    char IP1[50][10],cIP[4];
-    int i = 0,j=len-1;
-    int k, m = 0,n=0,num=0;
+    int len =argc;
+    char IP1[50][10] ;
+    char Port[20][10]  ;
+    int i = 0;
 
     printf("len%d:\n",len);
-    for (i; i <len-1; i++)
+    if (len <2) {
+        return 1;
+    }
+    for (i; i <len ; i++)
     {
-
-	if (strcmp(*(argv + i) ,"--ip") == 0)
-	{
-		//printf("argv:%s\n",*(argv + i+1));
-		strcpy((char*)IP1 , *(argv + i+1));
+	if ( strcmp(*(argv + i) ,"--ip") == 0) {
+		//printf("argv:%s\n",*(argv1 + i+1));
+		strcpy( ( char*)IP1 , *(argv + i+1));
+                Ipflag = 1;
 	}
+        if (strcmp(*(argv + i) ,"--port") == 0) {
+           // printf("Port:%s\n",*(argv1 + i+1));
+		strcpy((char*)Port , *(argv + i+1));
+                Portflag = 1;
+                portnum = atoi(*Port);
+        }
 
     }
-    int IPlen = strlen(*IP1);
-    for(int c=0;c<IPlen;c++){
-       // printf("IP1%d:%c\n",c,IP1[0][c]);
-    }
-    char *p = (char *)IP1;
 
+    if (!IP1 ) {
+        return 1;
+    }
+
+    paddr = (char *)*IP1;
+    pPort = (char *)*Port;
+    return 0;
+}
+
+
+int get_ip(uint32_t &addr) {
+	// TODO
+   int n=0,num=0,i=0;
+   char *p = paddr;
+   char cIP[4];
+   int sumbuf[20] = {0};
+   uint32_t ip;
+    if (!paddr) {
+        return 1;
+    }
     while (*p!= '\0')
     {
+
         if (*p == ' ' || *p<'0' || *p>'9') {
             return 1;
         }
         cIP[n++] = *p; //保存每个子段的第一个字符，用于之后判断该子段是否为0开头
         int sum = 0;  //sum为每一子段的数值，应在0到255之间
+
         while (*p != '.'&&*p != '\0')
         {
           if (*p == ' ' || *p<'0' || *p>'9') {
@@ -57,29 +86,46 @@ int arg_parse(int argc, const char **argv)
         };
         /* 若子段的值>255或为0开头的非0子段或“.”的数目>3，则为无效IP  */
         if ((sum > 255) || (sum > 0 && cIP[0] =='0')||num>3) {
-            return -1;
+            return 1;
         }
 
         if (*p != '\0') {
             p++;
         }
         n = 0;
+       sumbuf[i] = sum;
+       i++;
+       // printf ("sum:%d\n",sum);
     }
     if (num != 3) {
         return 1;
-   }
-	return 0;
-}
-
-
-int get_ip(uint32_t &addr)
-{
-	// TODO
+    }
+    addr = ( sumbuf[0]  << 24)+ ( sumbuf[1]  << 16) + ( sumbuf[2]  << 8) +  ( sumbuf[3]  << 0);
+        //addr =  (uint32_t)*paddr (s1 << 24) + (s2 << 8) + (s3 << 0);
+        //printf("addr:%x\n",addr);
 	return 0;
 }
 
 int get_port(uint32_t &port)
 {
 	// TODO
+        char *p = pPort;
+        int sum =0;
+        if ( Portflag== 0 ) {
+            printf("potr null");
+            return 1;
+        }
+        if (!portnum){
+            return 1;
+        }else {
+           // sum = atoi(p);
+            if ((portnum > 1) && (portnum < 65535)) {
+                port =  (uint32_t)portnum;
+            }
+            else {
+               return 1;
+            }
+        }
+       //printf("getport%d : ",portnum);
 	return 0;
 }
